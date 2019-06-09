62f60ef7dbf1f49b0d1d47061937ed655e06009f
root <guilhermebromonschenkel@gmail.com>
2019-06-09 16:49:37 -0300

    Published on github pages

---
+++ precache-manifest.4f33bf9a053c8d52be37520b6b41ecba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d24c6d4aa349ec699c30c969a125f09",
+    "url": "/PICSIZE-Challenge/index.html"
+  },
+  {
+    "revision": "9073bff2a8396754624a",
+    "url": "/PICSIZE-Challenge/static/css/main.d7893c9e.chunk.css"
+  },
+  {
+    "revision": "d60a8bdf9cefb8207edd",
+    "url": "/PICSIZE-Challenge/static/js/2.9d870939.chunk.js"
+  },
+  {
+    "revision": "9073bff2a8396754624a",
+    "url": "/PICSIZE-Challenge/static/js/main.a235c3b5.chunk.js"
+  },
+  {
+    "revision": "c7dd89938bb22c2b30b7",
+    "url": "/PICSIZE-Challenge/static/js/runtime~main.67279277.js"
+  }
+]);
