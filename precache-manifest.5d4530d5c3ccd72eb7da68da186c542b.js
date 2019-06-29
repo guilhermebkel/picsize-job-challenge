92d6eb5187ddd13baa63df12d9645e57782881c6
Guilherme <guilhermebromonschenkel@gmail.com>
2019-06-28 23:36:29 -0300

    Updates

---
+++ precache-manifest.5d4530d5c3ccd72eb7da68da186c542b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05191911c82e089de2c6dd92912a7e88",
+    "url": "/PICSIZE-Challenge/index.html"
+  },
+  {
+    "revision": "9d3ce66b87590c210800",
+    "url": "/PICSIZE-Challenge/static/css/main.d7893c9e.chunk.css"
+  },
+  {
+    "revision": "bfd6d060a5acb924f307",
+    "url": "/PICSIZE-Challenge/static/js/2.bf6dad59.chunk.js"
+  },
+  {
+    "revision": "9d3ce66b87590c210800",
+    "url": "/PICSIZE-Challenge/static/js/main.74ea2dbb.chunk.js"
+  },
+  {
+    "revision": "c7dd89938bb22c2b30b7",
+    "url": "/PICSIZE-Challenge/static/js/runtime~main.67279277.js"
+  }
+]);
